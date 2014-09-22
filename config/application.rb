df2445ff4bcdf7142eda302cd27f14850ab81f7c
Louis Fourrier <louis.fourrier@gmail.com>
2014-09-22 16:45:04 +0200

    assets problem solved

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ module Textcounter
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     
+    
     config.i18n.default_locale = :fr
     config.i18n.available_locales = [:fr, :en]
   end
